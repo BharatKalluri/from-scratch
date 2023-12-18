bd0c92feb00075e4bd1a380b6eb1e24c949a9182
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-12-18 17:53:56 +0530

    Added banner & head config

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,22 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>From scratch</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/bharatkalluri/from-scratch",
   },
   docsRepositoryBase: "https://github.com/bharatkalluri/from-scratch",
   footer: {
     text: "Hand crafted by Bharat Kalluri",
   },
+  useNextSeoProps: () => {
+    return {
+      titleTemplate: '%s – From Scratch'
+    }
+  },
+  banner: {
+    dismissible: true,
+    key: 'isJsonParserBannerDismissed',
+    text: '🎉 JSON parser from scratch chapter is not out!'
+  }
 };
 
 export default config;
