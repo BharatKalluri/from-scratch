40b8d1c89dfd21d71b485be9e905764afc2e7d09
Bharat Kalluri <bharatkalluri@protonmail.com>
2023-12-19 11:31:58 +0530

    OG headers fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,14 +12,35 @@ const config: DocsThemeConfig = {
   },
   useNextSeoProps: () => {
     return {
-      titleTemplate: '%s – From Scratch'
-    }
+      titleTemplate: "%s – From Scratch",
+    };
   },
+  head: () => (
+    <>
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="apple-mobile-web-app-title" content="From scratch" />
+
+      {['og:site_name', 'og:title', 'twitter:title'].map((property) => (
+        <meta key={property} property={property} content={'From scratch'} />
+      ))}
+      {['description', 'og:description', 'twitter:description'].map((property) => (
+        <meta key={property} name={property} content={'Building everyday technology, from scratch'} />
+      ))}
+      <meta name="twitter:site:domain" content="fromscratch.bharatkalluri.com" />
+      <meta name="twitter:url" content="https://x.com/the_bharat_k" />
+    </>
+  ),
   banner: {
     dismissible: true,
-    key: 'isJsonParserBannerDismissed',
-    text: '🎉 JSON parser from scratch chapter is now out!'
-  }
+    key: "isJsonParserBannerDismissed",
+    text: "🎉 JSON parser from scratch chapter is now out!",
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    autoCollapse: true,
+  },
 };
 
 export default config;
